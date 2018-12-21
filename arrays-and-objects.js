cc7328ddba2036572cf1107e92225adb6db4f249
Vash the red <42741076+vashthered@users.noreply.github.com>
2018-12-21 19:24:08 +0100

    New exercise added

--- arrays-and-objects.js
+++ arrays-and-objects.js
@@ -74,3 +74,65 @@ console.log(reverseArrayInPlace([3,2,1]));
 
 
 /*****************************************************************************************************************/
+
+//Create a list object from an array, the object must be like this:
+//var list = {
+//  value : 1,
+//  rest : {
+//    value : 2,
+//    rest : {
+//      value : 3,
+//      rest : null
+//    }
+//  }
+//};
+
+function arrayToList(array) {
+
+  var list = null;
+
+  for (var k = array.length - 1; k >= 0; k--) {
+
+    list = {value : array[k], rest : list}
+
+  }
+
+  return list;
+
+}
+
+console.log(arrayToList([12,43,54,65]));
+
+
+/*****************************************************************************************************************/
+
+//Match two objects
+
+function deepEqual(a, b) {
+
+  if (a === b) return true;
+
+  if (a == null || typeof a != "object" || b == null || typeof b != "object") return false;
+
+  let keysA = Object.keys(a), keysB = Object.keys(b);
+
+  if (keysA.length != keysB.length) return false;
+
+  for (let key of keysA) {
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+  }
+
+  return true;
+}
+
+var objTest1 = {
+  value : "abaco",
+  terminal : 123
+}
+
+var objTest2 = {
+  value : "abaco",
+  terminal : 123
+}
+
+console.log(deepEqual(objTest1, objTest2));
